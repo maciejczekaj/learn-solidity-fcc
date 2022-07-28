141719ed71e4e2fe48f8740bab7eea9f29da2a0a
Maciej Czekaj <mczekaj@valuelogic.one>
2022-07-28 15:37:58 +0200

    small typescript improvements

--- hardhat-simple-storage/tasks/block-number.ts
+++ hardhat-simple-storage/tasks/block-number.ts
@@ -1,5 +1,5 @@
 import { task } from 'hardhat/config';
 
-task('block-number', 'Prints the current block number').setAction(async (_, hre) =>
+export default task('block-number', 'Prints the current block number').setAction(async (_, hre) =>
     console.log(`Current block number: ${await hre.ethers.provider.getBlockNumber()}`)
 );
--- hardhat-simple-storage/test/simpleStorageTests.ts
+++ hardhat-simple-storage/test/simpleStorageTests.ts
@@ -1,20 +1,21 @@
 import { ethers } from 'hardhat';
 import { expect, assert } from 'chai';
+import { SimpleStorage, SimpleStorage__factory } from '../typechain-types';
 
 describe('SimpleStorage', () => {
-    let simpleStorage;
+    let simpleStorage: SimpleStorage;
 
-    beforeEach(async function() {
-        const simpleStorageFactory = await ethers.getContractFactory('SimpleStorage');
+    beforeEach(async function () {
+        const simpleStorageFactory = (await ethers.getContractFactory('SimpleStorage')) as SimpleStorage__factory;
         simpleStorage = await simpleStorageFactory.deploy();
     });
 
-    it('Should store 0 as favorite number after deploy', async function() {
+    it('Should store 0 as favorite number after deploy', async function () {
         const currentValue = await simpleStorage.retrieve();
         assert.equal(currentValue.toString(), '0');
     });
 
-    it('Should update when we call store', async function() {
+    it('Should update when we call store', async function () {
         const expectedValue = '7';
         const transactionResponse = await simpleStorage.store(7);
         await transactionResponse.wait(1);
@@ -23,11 +24,11 @@ describe('SimpleStorage', () => {
         expect(currentValue.toString()).to.equal(expectedValue);
     });
 
-    it('Should store person\'s favorite number', async function() {
+    it("Should store person's favorite number", async function () {
         const transactionResponse = await simpleStorage.addPerson('John', 2);
         await transactionResponse.wait(1);
 
         const johnsFavoriteNumber = await simpleStorage.nameToFavoriteNumber('John');
         expect(johnsFavoriteNumber.toString()).to.equal('2');
-    })
+    });
 });
